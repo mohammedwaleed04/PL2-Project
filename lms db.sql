8c18d92413024fd13a5f2badc23e5bc195d0760c
mohammedwaleed04 <90641420+mohammedwaleed04@users.noreply.github.com>
2022-12-10 14:02:14 +0200

    Update lms db.sql

--- lms db.sql
+++ lms db.sql
@@ -8,6 +8,7 @@ create table student(
     age int not null,
     email char(50) unique not null,
     password char(50) not null,
+    gender char(50) not null,
     semester int not null
 );
 alter table student auto_increment = 202100;
@@ -15,5 +16,6 @@ alter table student auto_increment = 202100;
 create table teacher(
 	email char(50) primary key not null,
     password char(50) not null,
+    gender char(50) not null,
     name char(50) not null
 );
