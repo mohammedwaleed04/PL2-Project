d5eeb6da69972b7c8daa85f46b25c37544c35e49
Mohamed Mostafa <91856839+muhammed-elsherif@users.noreply.github.com>
2022-12-19 15:08:28 -0800

    Add files via upload

---
+++ lmsDB.sql
@@ -0,0 +1,69 @@
+create database lmsdb;
+
+use lmsdb;
+
+create table teacher(
+	email char(50) primary key not null,
+    password char(50) not null,
+    name char(50) not null,
+    gender char(50) not null
+);
+
+create table users(
+	name char(50) not null,
+    email char(50) primary key not null,
+    password char(50),
+    teacher boolean,
+    admin boolean,
+    gender char(50) not null
+);
+
+create table LectureTable(
+WeekNO int primary key not null,
+courseTittle1 nvarchar(50),
+courseTittle2 nvarchar(50),
+courseTittle3 nvarchar(50),
+courseTittle4 nvarchar(50),
+Content nvarchar(50)
+);
+
+create table assign(
+week_no int,
+course nvarchar(50),
+assign_path varchar(50),
+assign_tittle varchar(50),
+id int primary key auto_increment
+);
+
+create table lecutretable(
+week_no int,
+course varchar(50),
+lec_path varchar(50),
+lec_tittle varchar(50),
+id int primary key auto_increment
+);
+
+select * from users;
+select * from student;
+select * from teacher;
+
+
+create table student(
+	id int primary key auto_increment,
+    name char(50) not null,
+    email char(50) unique not null,
+    password char(50) not null,
+    semester int not null,
+    gender char(50) not null,
+    pl int,
+    ds int,
+    logic int,
+    maths int,
+    course1 nvarchar(50) default'-',
+course2 nvarchar(50) default'-',
+course3 nvarchar(50) default'-',
+course4 nvarchar(50) default'-'
+);
+alter table student auto_increment = 202100;
+
+drop database lmsdb;
